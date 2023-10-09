109fd451bd8be516bce696321e6bc40d4a1c09d8
ERR0RPR0MPT <github@fwder.cn>
2023-10-09 19:32:30 +0800

    Chore: 构建目录设置到 ./ui

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ui/",
+  build: {
+    outDir: "ui",
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
